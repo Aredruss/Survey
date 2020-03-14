4f3ccc58757595fb99a45aa2d37b8bb7db96c585
Aredruss <a.e.medyanik@yandex.ru>
2020-03-14 21:20:50 +0300

    Basic Logic & Layout

--- app/src/main/java/com/redbox/survey/MainFragment.kt
+++ app/src/main/java/com/redbox/survey/MainFragment.kt
@@ -1,2 +1,26 @@
 package com.redbox.survey
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        start_test_btn.setOnClickListener {
+            val manager = activity!!.supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment, QuestionFragment()).addToBackStack("test").commit()
+        }
+    }
+}
